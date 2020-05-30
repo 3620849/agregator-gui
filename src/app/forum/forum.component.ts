ddecda7a447c1e23cc2003cc3ea071e8c968e017
Dima <3620849@gmail.com>
2020-05-30 21:26:29 +0300

    fix bug internal load

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { SiteOptionsComponent } from './site-options/site-options.component';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
 
@@ -15,17 +15,18 @@ export class ForumComponent implements OnInit {
   tabs={top:0,new:1,monthly:2}
   dialogRef: any;
   state$: Observable<object>;
-  tab=this.tabs.top;
-  currentIndex=0;
-  pageName='top';
+ // tab=this.tabs.top;
+  //currentIndex=0;
+  pageName:BehaviorSubject<string>=new BehaviorSubject('top');
   constructor(public dialog: MatDialog,private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.state$ = this.activatedRoute.paramMap
       .pipe(map(() => window.history.state));
       this.state$.subscribe(state=>{
-        if(state['tab']){
-          this.tab = this.tabs[state['tab']];
+        if(state['tab']){ 
+          //this.tab = this.tabs[state['tab']];
+          this.pageName.next(state['tab']);
         }
       });
   }
@@ -41,6 +42,6 @@ export class ForumComponent implements OnInit {
     this.dialogRef.close();
   }
   switchList(listName:string){
-    this.pageName = listName;
+    this.pageName.next(listName);
   }
 }
--- src/app/forum/post-feed/post-feed.component.ts
+++ src/app/forum/post-feed/post-feed.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit, Input,AfterContentInit} from '@angular/core';
+import { Component, OnInit, Input, AfterContentInit } from '@angular/core';
 import { PostFeedService } from './post-feed.service';
+import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-post-feed',
   templateUrl: './post-feed.component.html',
   styleUrls: ['./post-feed.component.scss']
 })
-export class PostFeedComponent  {
+export class PostFeedComponent {
   @Input()
-  pageName:string;
-  listOfPost=[];
-  skip="0";
+  pageName: BehaviorSubject<string>;
+  currentPageName;
+  listOfPost = [];
+  skip = "0";
 
-  constructor(private postService:PostFeedService) { }
+  constructor(private postService: PostFeedService) { }
 
   ngOnInit(): void {
-     
+    this.pageName.subscribe(page => {
+       
+      this.currentPageName = page;
+      this.listOfPost = [];
+      this.skip = "0";
+      //this.loadPost();
+    })
   }
-  loadPost(){
-    this.postService.getPostFeed(this.pageName,this.skip).subscribe(response=>{
-      this.listOfPost=[...this.listOfPost,...response['messageList']]; 
-      this.skip=""+this.listOfPost.length;
-   })
+  loadPost() {
+    console.log("load post")
+    if (this.currentPageName) {
+      this.postService.getPostFeed(this.currentPageName, this.skip).subscribe(response => {
+        this.listOfPost = [...this.listOfPost, ...response['messageList']];
+        this.skip = "" + this.listOfPost.length;
+      })
+    }
   }
 
 }
